4cbd11cdcd4de5f6daa38f751ca4d0f101612d29
Crypt <84793235+MercuryHg12@users.noreply.github.com>
2023-09-17 19:48:47 +0300

    Updated response events + Added Custom icons for Dialogue Objects

--- Osaka/Assets/Scripts/UI/Dialogue/DialogueActivator.cs
+++ Osaka/Assets/Scripts/UI/Dialogue/DialogueActivator.cs
@@ -33,9 +33,13 @@ public class DialogueActivator : MonoBehaviour, IInteractable
 
     public void Interact(PlayerMovement player)
     {
-        if (TryGetComponent(out DialogueResponseEvents responseEvents) && responseEvents.DialogueObject == dialogueObject)
+        foreach (DialogueResponseEvents responseEvents in GetComponents<DialogueResponseEvents>())
         {
-            player.DialogueUI.AddResponseEvents(responseEvents.Events);
+            if (responseEvents.DialogueObject == dialogueObject)
+            {
+                player.DialogueUI.AddResponseEvents(responseEvents.Events);
+                break;
+            }
         }
 
         player.DialogueUI.ShowDialogue(dialogueObject);
